ab711b01b2f3d97400db92958435501a828aa563
Egor Ivanov <rust_lang_lover@proton.me>
2024-05-06 13:07:32 +0300

    // added logging when note is added to order

--- src/axum_routes/routes/orders_routes/add_note_to_order/add_note_to_order_sql.rs
+++ src/axum_routes/routes/orders_routes/add_note_to_order/add_note_to_order_sql.rs
@@ -1,6 +1,7 @@
 use mysql::{Error, params, PooledConn};
 use mysql::prelude::Queryable;
 use tokio::sync::MutexGuard;
+use crate::axum_routes::generic_replies::generic_log_writer::generic_log_writer;
 use crate::axum_routes::routes::orders_routes::add_note_to_order::add_note_to_order_structs::InsertStruct;
 use crate::structs::structs::NoteObjectNotation;
 
@@ -12,7 +13,7 @@ pub fn add_note_to_order_sql(unlocked : &mut MutexGuard<PooledConn>, id : u16, n
     let mut to_insert : Vec<InsertStruct> = Vec::with_capacity(1);
     to_insert.push(InsertStruct {
         id,
-        note,
+        note : note.clone(),
     });
     match unlocked.exec_batch(r"INSERT INTO order_notes VALUES (0, :related_id, :text_info, NOW())",
                               to_insert.iter().map(|value| params! {
@@ -21,19 +22,26 @@ pub fn add_note_to_order_sql(unlocked : &mut MutexGuard<PooledConn>, id : u16, n
         })
     ) {
         Ok(_) => {
-            match unlocked.query_map(format_notes_parsing(id),
-                                     |(id, text_info, date_time)| {
-                                         NoteObjectNotation {
-                                             id,
-                                             text_info,
-                                             date_time
-                                         }
-                                     })
-            {
-                Ok(value) => {
-                    return Ok(value)
+            match generic_log_writer(format!("Добавлена пометка для заявки номер : {} -> '{}'", id, note),unlocked) {
+                Ok(_) => {
+                    match unlocked.query_map(format_notes_parsing(id),
+                                             |(id, text_info, date_time)| {
+                                                 NoteObjectNotation {
+                                                     id,
+                                                     text_info,
+                                                     date_time
+                                                 }
+                                             })
+                    {
+                        Ok(value) => {
+                            return Ok(value)
+                        }
+                        Err(e) => {return Err(e)}
+                    }
                 }
-                Err(e) => {return Err(e)}
+                Err(e) => {
+                    return Err(e)
+                }
             }
         }
         Err(e) => {return Err(e)}
