d32b104c7e171c8656185d42ec13be85ad3c7449
Egor Ivanov <rust_lang_lover@proton.me>
2024-05-06 12:18:54 +0300

    // added logging when changing the order status

--- src/axum_routes/routes/orders_routes/change_status_by_id/change_status_by_id_sql.rs
+++ src/axum_routes/routes/orders_routes/change_status_by_id/change_status_by_id_sql.rs
@@ -1,13 +1,14 @@
 use mysql::{Error, params, PooledConn};
 use mysql::prelude::Queryable;
 use tokio::sync::MutexGuard;
+use crate::axum_routes::generic_replies::generic_log_writer::generic_log_writer;
 use crate::axum_routes::routes::orders_routes::change_status_by_id::change_status_by_id_structs::InsertStruct;
 
 pub fn change_status_by_id_sql(unlocked : &mut MutexGuard<PooledConn>, id : u16, new_status : String) -> mysql::Result<(), Error> {
     let mut to_insert : Vec<InsertStruct> = Vec::with_capacity(1);
     to_insert.push(InsertStruct {
         id,
-        new_status,
+        new_status : new_status.clone(),
     });
     match unlocked.exec_batch(r"UPDATE ugo_customers_request SET request_status = :status WHERE id = :id",
                               to_insert.iter().map(|value| params! {
@@ -16,7 +17,14 @@ pub fn change_status_by_id_sql(unlocked : &mut MutexGuard<PooledConn>, id : u16,
         })
     ) {
         Ok(_) => {
-            return Ok(())
+            match generic_log_writer(format!("Изменен статус для заявки номер : {} на '{}'", id, new_status), unlocked) {
+                Ok(_) => {
+                    return Ok(())
+                }
+                Err(e) => {
+                    return Err(e)
+                }
+            }
         }
         Err(e) => {
             return Err(e)
