ca837596213a387296d4476801d945ec8b2ada33
Egor Ivanov <rust_lang_lover@proton.me>
2024-05-09 16:11:32 +0300

    // comments

--- src/structs/constants.rs
+++ src/structs/constants.rs
@@ -7,5 +7,6 @@ pub const FILE_LOCATION : &'static str = r#"C:\Users\User\Desktop\mysql.txt"#;
 
 // https://ugo-vape.ru
 // http://localhost:8000
+// http://localhost:3000
 // Replace cors route here! + check if "/" at the end of URL, it shouldn't be there!
 pub const CORS_ROUTE : &'static str = "http://localhost:3000";
