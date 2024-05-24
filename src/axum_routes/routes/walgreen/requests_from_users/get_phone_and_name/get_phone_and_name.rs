dfc5287c66b5bb01e33cebf66acd2b5542362641
rust_lang_lover <rust_lang_lover@proton.me>
2024-05-24 14:48:57 +0300

    // changed writing functions for WGL

--- src/axum_routes/routes/walgreen/requests_from_users/get_phone_and_name/get_phone_and_name.rs
+++ src/axum_routes/routes/walgreen/requests_from_users/get_phone_and_name/get_phone_and_name.rs
@@ -4,10 +4,10 @@ use axum::{Extension, Json};
 use axum::response::IntoResponse;
 use mysql::PooledConn;
 use crate::axum_routes::generic_replies::generic_replies::reply_with_message;
+use crate::axum_routes::routes::ugo_vape::orders_routes::write_route::write_route_structs::WriteDataBody;
 use crate::axum_routes::routes::walgreen::requests_from_users::get_phone_and_name::get_phone_and_name_sql::get_phone_and_name_sql;
-use crate::axum_routes::routes::walgreen::requests_from_users::get_phone_and_name::get_phone_and_name_structs::GetPhoneAndName;
 
-pub async fn get_phone_and_name(pool : Extension<Arc<Mutex<PooledConn>>>, Json(body) : Json<GetPhoneAndName>) -> impl IntoResponse {
+pub async fn get_phone_and_name(pool : Extension<Arc<Mutex<PooledConn>>>, Json(body) : Json<WriteDataBody>) -> impl IntoResponse {
     let mut unlocked = pool.lock().await;
     match get_phone_and_name_sql(&mut unlocked, [body]) {
         Ok(_) => {
--- src/axum_routes/routes/walgreen/requests_from_users/get_phone_and_name/get_phone_and_name_sql.rs
+++ src/axum_routes/routes/walgreen/requests_from_users/get_phone_and_name/get_phone_and_name_sql.rs
@@ -1,15 +1,16 @@
 use mysql::{Error, params, PooledConn};
 use mysql::prelude::Queryable;
 use tokio::sync::MutexGuard;
-use crate::axum_routes::routes::walgreen::requests_from_users::get_phone_and_name::get_phone_and_name_structs::GetPhoneAndName;
+use crate::axum_routes::routes::ugo_vape::orders_routes::write_route::write_route_structs::WriteDataBody;
 
-pub fn get_phone_and_name_sql(pool : &mut MutexGuard<PooledConn>, body : [GetPhoneAndName ; 1]) -> mysql::Result<(), Error> {
-    match pool.exec_batch(r"INSERT INTO walgreen_customers_request VALUES (:id, :customer_name, :customer_phone_email, :customer_comment, NOW())",
+pub fn get_phone_and_name_sql(pool : &mut MutexGuard<PooledConn>, body : [WriteDataBody ; 1]) -> mysql::Result<(), Error> {
+    match pool.exec_batch(r"INSERT INTO walgreen_customers_request VALUES (:id, :request_status, :customer_name, :customer_email, :customer_self_description, NOW())",
         body.iter().map(|value| params! {
             "id" => 0,
-            "customer_name" => &value.customer_name,
-            "customer_phone_email" => &value.customer_phone_email,
-            "customer_comment" => &value.customer_comment
+            "request_status" => "БЕЗ ВНИМАНИЯ".to_string(),
+            "customer_name" => &value.name,
+            "customer_email" => &value.email,
+            "customer_self_description" => &value.about_customer
         })
     ) {
         Ok(_) => {
