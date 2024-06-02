c1252a8a81c78edd43f0f94e79ac60f831ae483e
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-02 14:27:48 +0300

    // finished port checker test

--- src/structs/constants.rs
+++ src/structs/constants.rs
@@ -1,6 +1,6 @@
-pub const SESSION_DURATION : u16 = 900; // duration of login session in seconds
+pub const SESSION_DURATION : u16 = 900; // duration of session after login in seconds
 
-pub const DEPLOY_PORT : u16 = 8000; // port to deploy an API
+pub const DEPLOY_PORT : u16 = 8000; // local port to deploy an API
 
 // /Users/egorivanov/Desktop/mysql.txt - MacOS
 // C:\Users\User\Desktop\mysql.txt - Windows
@@ -10,5 +10,5 @@ pub const FILE_LOCATION : &'static str = r#"C:\Users\User\Desktop\mysql.txt"#;
 // https://ugo-vape.ru
 // http://localhost:8000
 // http://localhost:3000
-// Replace cors route here! + check if "/" at the end of URL, it shouldn't be there!
+// Replace cors route here! + check if there is a "/" at the end of URL, it shouldn't be there!
 pub const CORS_ROUTE : &'static str = "http://localhost:3000";
--- src/tests/check_if_port_is_open.rs
+++ src/tests/check_if_port_is_open.rs
@@ -1,10 +1,9 @@
-use port_scanner::scan_port;
 use crate::structs::constants::DEPLOY_PORT;
 
 #[test]
 fn check_if_port_is_open() -> Result<(), String> {
-    return match scan_port(DEPLOY_PORT) {
-        true => { Ok(()) }
-        false => { Err(format!("Port {} is occupied, please pick another one!", DEPLOY_PORT)) }
+    match std::net::TcpListener::bind(format!("0.0.0.0:{}", DEPLOY_PORT)) {
+        Ok(_) => {Ok(())}
+        Err(err) => {Err(err.to_string())}
     }
 }
