52e653104571f5955d7d1773d5c45642f44c03ba
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-02 14:51:57 +0300

    // comments

--- src/tests/check_mysql_file_location.rs
+++ src/tests/check_mysql_file_location.rs
@@ -1,5 +1,6 @@
 use crate::structs::constants::FILE_LOCATION;
 
+// Check the location of file before connecting to the db
 #[test]
 fn check_mysql_file_location() -> Result<(), String> {
     return match std::fs::read_to_string(FILE_LOCATION) {
