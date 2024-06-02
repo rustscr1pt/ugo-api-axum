510536e15e4c9b632a0e5340ae5225b713f30a30
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-02 14:51:14 +0300

    // comments

--- src/tests/check_internet_connection.rs
+++ src/tests/check_internet_connection.rs
@@ -1,6 +1,7 @@
 use std::fmt::Display;
 use online::check;
 
+// Check if it's possible to establish connection to the web
 #[test]
 fn check_internet_connection() -> Result<(), String> {
     return match check(None).is_ok() {
