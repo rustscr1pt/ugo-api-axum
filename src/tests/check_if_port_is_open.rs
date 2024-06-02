47bd48aad95631f05f7609df800815f9b8cff99c
Egor Ivanov <rust_lang_lover@proton.me>
2024-06-02 14:50:51 +0300

    // comments

--- src/tests/check_if_port_is_open.rs
+++ src/tests/check_if_port_is_open.rs
@@ -1,5 +1,6 @@
 use crate::structs::constants::DEPLOY_PORT;
 
+// Check if it's possible to use port for deploying an API
 #[test]
 fn check_if_port_is_open() -> Result<(), String> {
     match std::net::TcpListener::bind(format!("0.0.0.0:{}", DEPLOY_PORT)) {
