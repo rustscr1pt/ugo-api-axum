b7f75006a3db2558da7446f911981c30f3967e82
rust_lang_lover <rust_lang_lover@proton.me>
2024-06-10 13:20:03 +0300

    // test fix

--- src/tests/check_connection_mysql.rs
+++ src/tests/check_connection_mysql.rs
@@ -10,7 +10,10 @@ fn check_connection_mysql() -> Result<(), String> {
             match Pool::new(result.trim()) {
                 Ok(pool) => {
                     match pool.get_conn() {
-                        Ok(_) => { Ok(()) }
+                        Ok(connection) => {
+                            drop(connection);
+                            Ok(())
+                        }
                         Err(err) => { Err(err.to_string()) }
                     }
                 }
