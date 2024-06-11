178850401fae6fb95e4ebe2db3bcb1e9b1e2c35c
rust_lang_lover <rust_lang_lover@proton.me>
2024-06-11 12:32:04 +0300

    // comments

--- src/generic_replies/generic_log_writer.rs
+++ src/generic_replies/generic_log_writer.rs
@@ -3,6 +3,8 @@ use mysql::{params, PooledConn};
 use mysql::prelude::Queryable;
 use tokio::sync::MutexGuard;
 
+
+// Write logs to the mySQL db
 pub fn generic_log_writer<T : Display>(log : T, pool : &mut MutexGuard<PooledConn>) -> Result<(), mysql::Error> {
     let iterable : [T ; 1] = [log];
     match pool.exec_batch(r"INSERT INTO ugo_logs VALUES (0, :contents, NOW())",
