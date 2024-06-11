4579f5dd2ee6f6b2e79ba1fe677c517781328069
rust_lang_lover <rust_lang_lover@proton.me>
2024-06-11 12:46:23 +0300

    // comments

--- src/mysql/establish_connection.rs
+++ src/mysql/establish_connection.rs
@@ -2,6 +2,8 @@ use std::fs;
 use mysql::{Pool, PooledConn};
 use crate::structs::constants::FILE_LOCATION;
 
+// Establish connection with the db at the start of execution.
+
 pub fn establish_connection() -> PooledConn { // First action to check the connection and establish a working pool
     let pool = Pool::new(fs::read_to_string(FILE_LOCATION).unwrap().trim()).expect("Couldn't connect to a base");
     println!("Connection with MySQL pool is established!");
