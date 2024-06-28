18faecc6ad589d14aeb6c71974e600197de19173
rust_lang_lover <rust_lang_lover@proton.me>
2024-06-28 14:47:22 +0300

    // check_if_port_is_open.rs replaced number with constant

--- src/tests/check_if_port_is_open.rs
+++ src/tests/check_if_port_is_open.rs
@@ -1,9 +1,9 @@
-use crate::structs::constants::DEPLOY_PORT;
+use crate::structs::constants::{DEPLOY_PORT, STANDARD_IP};
 
 // Check if it's possible to use port for deploying an API
 #[test]
 fn check_if_port_is_open() -> Result<(), String> {
-    match std::net::TcpListener::bind(format!("0.0.0.0:{}", DEPLOY_PORT)) {
+    match std::net::TcpListener::bind(format!("{}:{}", STANDARD_IP, DEPLOY_PORT)) {
         Ok(_) => {Ok(())}
         Err(err) => {Err(err.to_string())}
     }
