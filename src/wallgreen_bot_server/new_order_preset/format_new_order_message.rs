c25817c2ca7208ccab74c1030b962abe243439c4
rust_lang_lover <rust_lang_lover@proton.me>
2024-08-08 14:22:46 +0300

    // added a link to the react panel in telegram notification message

--- src/wallgreen_bot_server/new_order_preset/format_new_order_message.rs
+++ src/wallgreen_bot_server/new_order_preset/format_new_order_message.rs
@@ -3,6 +3,6 @@ use crate::wallgreen_bot_server::new_order_preset::base_selector_enum::BaseSelec
 use crate::wallgreen_bot_server::new_order_preset::format_the_source::format_the_source;
 
 pub fn format_new_order_message(object : &BasicPartGetAll, source : BaseSelector) -> String {
-    return format!("Принята новая заявка с сайта : {}\n\nПорядковый номер : {}\nУстановлен статус : {}\n\nИмя заказчика : {}\nПочтовый адрес : {}\nОписание заявки : {}\n\nВремя добавления : {}",
+    return format!("Принята новая заявка с сайта : {}\n\nПорядковый номер : {}\nУстановлен статус : {}\n\nИмя заказчика : {}\nПочтовый адрес : {}\nОписание заявки : {}\n\nВремя добавления : {}\n\nБольше информации в панели по ссылке : https://ugo-vape.ru/__admin-panel/",
                    format_the_source(source), object.id, object.request_status, object.customer_name, object.customer_email, object.customer_self_description, object.date_time_added)
 }
